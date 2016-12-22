81841ca0c8d71ba5d82eed9fef7141838cb9e3b7
Mieszko Makuch <mieszkomakuch@gmail.com>
2016-12-22 18:07:16 +0100

    Updated SejmometrCreator (support for json pagination)

--- src/main/java/agh/cs/lab9/json/SejmometrCreator.java
+++ src/main/java/agh/cs/lab9/json/SejmometrCreator.java
@@ -5,39 +5,39 @@ import agh.cs.lab9.json.sejmometr.Dataobject;
 import agh.cs.lab9.json.sejmometr.SejmometrAPI;
 import com.google.gson.Gson;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by mieszkomakuch on 15.12.2016.
  */
 public class SejmometrCreator extends AbstractCreator {
-    String siteUrl = "https://api-v3.mojepanstwo.pl/dane/poslowie.json?conditions[poslowie.kadencja]=";
-    int term;
+    boolean usingLocalFiles;
+    protected String url;
+    protected int term;
+    protected ArrayList<String> pagesUrls = new ArrayList<String>();
+    SejmometrAPI sejmometrAPI;
 
-    public SejmometrCreator(int term) {
+    public SejmometrCreator(String url, int term) {
+        boolean usingLocalFiles = false;
+        this.url = url;
         this.term = term;
+        sejmometrAPI = createMainSejmometrApiClass();
     }
 
-    protected String getUrl() {
-        return siteUrl + term;
-    }
+    protected String getUrl() {return url + term;}
 
     public Sejmometr createSejmometr() {
-        String jsonSejmometrAPI = null;
-        try {
-            jsonSejmometrAPI = getJSON(this.getUrl());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        SejmometrAPI sejmometrAPI = createSejmometrAPIClassFromUrl(this.getUrl());
-
-        sejmometrAPI = parseNextPages(sejmometrAPI);
-        return new Sejmometr(sejmometrAPI);
+        return new Sejmometr(sejmometrAPI, this.term);
     }
 
-    private SejmometrAPI createSejmometrAPIClassFromUrl (String url) {
+    protected SejmometrAPI createMainSejmometrApiClass() {
+        pagesUrls.add(getUrl());
+        SejmometrAPI sejmometrAPI = createSingleSejmometrAPIClassFromUrl(this.pagesUrls.get(0));
+        return parseNextPages(sejmometrAPI);
+    }
+
+    protected SejmometrAPI createSingleSejmometrAPIClassFromUrl (String url) {
         String jsonSejmometrAPI = null;
         try {
             jsonSejmometrAPI = getJSON(url);
@@ -47,18 +47,34 @@ public class SejmometrCreator extends AbstractCreator {
         return new Gson().fromJson(jsonSejmometrAPI, SejmometrAPI.class);
     }
 
-    private SejmometrAPI parseNextPages(SejmometrAPI firstPageAPI) {
-
+    protected SejmometrAPI parseNextPages(SejmometrAPI firstPageAPI) {
         List<Dataobject> allDataObjects = firstPageAPI.getDataobject();
         SejmometrAPI nextSejmometrAPI = firstPageAPI;
-
+        int currentPageNo = 1;
         while (nextSejmometrAPI.getLinks().getNext() != null) {
-            String nextPageUrl = nextSejmometrAPI.getLinks().getNext();
-            nextSejmometrAPI = createSejmometrAPIClassFromUrl(nextPageUrl);
+            String nextPageUrl = getNextPageUrl(nextSejmometrAPI, currentPageNo);
+            pagesUrls.add(nextPageUrl);
+            nextSejmometrAPI = createSingleSejmometrAPIClassFromUrl(nextPageUrl);
             allDataObjects.addAll(nextSejmometrAPI.getDataobject());
+            currentPageNo++;
         }
         firstPageAPI.setDataobject(allDataObjects);
         return firstPageAPI;
     }
 
+    protected String getNextPageUrl(SejmometrAPI sejmometrAPI, int currentPage) {
+        return sejmometrAPI.getLinks().getNext();
+    }
+
+    public ArrayList<String> getPagesUrls() {
+        return this.pagesUrls;
+    }
+
+    public ArrayList<Integer> getRepresentativesIds(){
+        ArrayList<Integer> representativesIds = new ArrayList<Integer>();
+        for (Dataobject dataobject : sejmometrAPI.getDataobject()) {
+            representativesIds.add(dataobject.getId());
+        }
+        return representativesIds;
+    }
 }
