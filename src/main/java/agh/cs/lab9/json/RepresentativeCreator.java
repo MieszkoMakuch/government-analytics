f1fdf3662f7cf57101d6adaa41ced15376372fa3
Mieszko Makuch <mieszkomakuch@gmail.com>
2016-12-22 18:05:20 +0100

    Updated class: RepresentativeCreator (support for LocalRepresentativeCreator, support for Representatives without trips details)

--- src/main/java/agh/cs/lab9/json/RepresentativeCreator.java
+++ src/main/java/agh/cs/lab9/json/RepresentativeCreator.java
@@ -1,37 +1,35 @@
 package agh.cs.lab9.json;
 
 import agh.cs.lab9.Representative;
-import agh.cs.lab9.RepresentativeWithTrips;
+import agh.cs.lab9.RepresentativeWithTripsInfo;
 import agh.cs.lab9.json.representative.RepresentativeAPI;
 import agh.cs.lab9.json.representative.spendings.RepresentativeSpendingsAPI;
 import agh.cs.lab9.json.representative.trips.RepresentativeTripsAPI;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 /**
  * Created by mieszkomakuch on 15.12.2016.
  */
 public class RepresentativeCreator extends AbstractCreator {
-    private String siteUrl = "https://api-v3.mojepanstwo.pl/dane/poslowie/";
-    int id;
-    private String extension = ".json";
-    private String layersInUrl = "?layers[]=krs&layers[]=";
-    private String tripsInUrl = "wyjazdy";
-    private String spendingsInUrl = "wydatki";
+    protected int id;
+    protected final String url;
 
-    public RepresentativeCreator(int id) {
+    public RepresentativeCreator(String url, int id) {
+        this.url = url;
         this.id = id;
     }
 
     protected String getUrl() {
-        return siteUrl + id + extension;
+        return url  + id + RepresentativeUpdater.extension;
     }
 
     protected String getTripsUrl() {
-        return siteUrl + id + extension + layersInUrl + tripsInUrl;
+        return getUrl() + RepresentativeUpdater.layersInUrl + RepresentativeUpdater.tripsInUrl;
     }
 
     protected String getSpendingsUrl(){
-        return siteUrl + id + extension + layersInUrl + spendingsInUrl;
+        return getUrl() + RepresentativeUpdater.layersInUrl + RepresentativeUpdater.spendingsInUrl;
     }
 
     public Representative createRepresentative() {
@@ -48,25 +46,26 @@ public class RepresentativeCreator extends AbstractCreator {
         RepresentativeAPI representativeAPI = gson.fromJson(jsonRepresentativeAPI, RepresentativeAPI.class);
         RepresentativeSpendingsAPI representativeSpendingsAPI = gson.fromJson(jsonRepresentativeSpendingsAPI, RepresentativeSpendingsAPI.class);
 
-        if(representativeAPI.getData().getPoslowieLiczbaWyjazdow() == 0){
-            return new Representative(
-                    representativeAPI,
-                    representativeSpendingsAPI);
-        } else {
+        if(representativeAPI.getData().getPoslowieLiczbaWyjazdow() > 0) {
             String jsonRepresentativeTripsAPI = null;
             try {
                 jsonRepresentativeTripsAPI = getJSON(this.getTripsUrl());
+
+                RepresentativeTripsAPI representativeTripsAPI = gson.fromJson(jsonRepresentativeTripsAPI, RepresentativeTripsAPI.class);
+                return new RepresentativeWithTripsInfo(
+                        representativeAPI,
+                        representativeTripsAPI,
+                        representativeSpendingsAPI);
+            } catch (JsonSyntaxException e) {
+                System.out.println("Brak informacji o wyjazdach posła o id: " + representativeAPI.getId());
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            RepresentativeTripsAPI representativeTripsAPI = gson.fromJson(jsonRepresentativeTripsAPI, RepresentativeTripsAPI.class);
-
-            return new RepresentativeWithTrips(
-                    representativeAPI,
-                    representativeTripsAPI,
-                    representativeSpendingsAPI);
         }
+        return new Representative(
+                representativeAPI,
+                representativeSpendingsAPI);
     }
 }
 
